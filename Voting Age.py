2085d000f923e40048659cda49254b8b5ac4f3a5
sean859 <sean.albertyn04@gmail.com>
2024-02-07 08:52:09 +1300

    Voting Age Verificaton
    Checks if the person is old enough to vote

---
+++ Voting Age.py
@@ -0,0 +1,18 @@
+def voter_eligibility(age):
+    return age >= 18
+
+def main():
+    try:
+        age = int(input("Input your age: "))
+        
+        if age < 0:
+            print("Age cannot be negative.")
+        elif voter_eligibility(age):
+            print("You are eligible to vote.")
+        else:
+            print("You are not eligible to vote.")
+    except ValueError:
+        print("Invalid input. Please input a valid age.")
+
+if __name__ == "__main__":
+    main()
