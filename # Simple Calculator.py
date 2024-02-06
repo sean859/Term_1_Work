d69d509ba380fe3c364d303d2a0298cda5296b61
sean859 <sean.albertyn04@gmail.com>
2024-02-07 08:39:11 +1300

    Calculator Finished
    The calculator is finished

--- # Simple Calculator.py
+++ # Simple Calculator.py
@@ -8,22 +8,22 @@ print("4. Divide")
 
 operation = input()
 
-if operation == 1:
+if operation == "1":
     number1 = input("Please enter your first number: ")
     number2 = input("Please enter your second number: ")
-    print("Adding these numbers together give you: " + number1 + number2)
-elif operation == 2:
+    print("Adding these numbers together give you: " + str(int(number1) + int(number2)))
+elif operation == "2":
     number1 = input("Please enter your first number: ")
     number2 = input("Please enter your second number: ")
-    print("Subtracting these numbers together give you: " + number1 - number2)
-elif operation == 3:
+    print("Subtract these numbers together give you: " + str(int(number1) - int(number2)))
+elif operation == "3":
     number1 = input("Please enter your first number: ")
     number2 = input("Please enter your second number: ")
-    print("Adding these numbers together give you: " + number1 + number2)
-elif operation == 4:
+    print("Multiply these numbers together give you: " + str(int(number1) * int(number2)))
+elif operation == "4":  
     number1 = input("Please enter your first number: ")
     number2 = input("Please enter your second number: ")
-    print("Adding these numbers together give you: " + number1 + number2)
+    print("Dividing these numbers together give you: " + str(int(number1) / int(number2)))
 else:
     print("Please put a valid number")
     
