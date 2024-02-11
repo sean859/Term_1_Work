7ec3dc2c0f03765d41aad0b23f94b5eb02ce4799
sean859 <sean.albertyn04@gmail.com>
2024-02-12 12:23:16 +1300

    Created Class and Objects
    Completed up to task 6 for Day 1 Excercises

---
+++ Class and Objects.py
@@ -0,0 +1,26 @@
+# Class Creation
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def __del__(self):
+        print("Deleted Everything")
+    
+    def start_engine(self):
+        print("The engine is starting")
+
+My_Car = Car("General Motors", "Chevrolet Corvette", 1985)
+Second_Car = Car("Nissan Motor Company", "Nissan Note", 2018)
+Third_Car = Car("Mazda", "Mazda Axela", 2015)
+print(My_Car.model)
+print(My_Car.make)
+print(My_Car.year)
+print(Second_Car.model)
+print(Second_Car.make)
+print(Second_Car.year)
+print(Third_Car.model)
+print(Third_Car.make)
+print(Third_Car.year)
+My_Car.start_engine()
