167d1cc38b544fd2e892b4b8a0737114fdde5491
sean859 <sean.albertyn04@gmail.com>
2024-02-12 12:34:30 +1300

    Update Class and Objects.py
    Smooth out the printing code

--- Class and Objects.py
+++ Class and Objects.py
@@ -5,19 +5,16 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+    def give_details(self):
+        print("Make:", self.make, "| Model:", self.model, "| Year:", self.year)
     def start_engine(self):
         print("The engine is starting")
 
 My_Car = Car("General Motors", "Chevrolet Corvette", 1985)
 Second_Car = Car("Nissan Motor Company", "Nissan Note", 2018)
 Third_Car = Car("Mazda", "Mazda Axela", 2015)
-print(My_Car.model)
-print(My_Car.make)
-print(My_Car.year)
-print(Second_Car.model)
-print(Second_Car.make)
-print(Second_Car.year)
-print(Third_Car.model)
-print(Third_Car.make)
-print(Third_Car.year)
+My_Car.give_details()
 My_Car.start_engine()
+Second_Car.give_details()
+Third_Car.give_details()
+
