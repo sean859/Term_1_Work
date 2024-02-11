bed837ad1fb686cdb4bb5f98473ff790ee99bf57
sean859 <sean.albertyn04@gmail.com>
2024-02-12 12:50:22 +1300

    Added Deconstructor
    However there is a bug where it prints the message three times instead of once

--- Class and Objects.py
+++ Class and Objects.py
@@ -1,13 +1,16 @@
 # Class Creation
 
 class Car:
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year): # Constructor
         self.make = make
         self.model = model
         self.year = year
-    def give_details(self):
+    def __del__(self): # Deconstructor
+        class_name = self.__class__.__name__
+        print(class_name, "was removed")
+    def give_details(self): # Print Details For Cars
         print("Make:", self.make, "| Model:", self.model, "| Year:", self.year)
-    def start_engine(self):
+    def start_engine(self): # Start the car engine
         print("The engine is starting")
 
 My_Car = Car("General Motors", "Chevrolet Corvette", 1985)
@@ -17,4 +20,4 @@ My_Car.give_details()
 My_Car.start_engine()
 Second_Car.give_details()
 Third_Car.give_details()
-
+del My_Car
