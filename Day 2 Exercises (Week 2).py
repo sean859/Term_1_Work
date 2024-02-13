236eab02e99e09548b5a01f83a495aa1befac9cd
sean859 <sean.albertyn04@gmail.com>
2024-02-14 12:52:32 +1300

    Completed task 1

--- Day 2 Exercises (Week 2).py
+++ Day 2 Exercises (Week 2).py
@@ -1,2 +1,14 @@
-# Singl Inheritence
+# Single Inheritence
 
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    def make_sound(self):
+        print("Random Animal Noise")
+class Dog(Animal):
+    def __init__(self, name, species,):
+        super().__init__(name, species)
+        print("The", self.species, "says Woof Woof")
+
+Pug = Dog("Jason", "Pug")
