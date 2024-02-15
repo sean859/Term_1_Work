e17347b44506be956fcc7a7c898dacf08a71c5cd
sean859 <sean.albertyn04@gmail.com>
2024-02-16 11:29:58 +1300

    Fixed up the add vender option
    Also added the update vender option

--- Day 4 and 5 Exercise (Week 2).py
+++ Day 4 and 5 Exercise (Week 2).py
@@ -10,11 +10,41 @@ class Vender:
             self.phone = phone
             self.email = email
         def print_vender(self):
-             print("Are These details correct?")
-             print("Name: " + self.name)
-             print("Phone: " + self.phone)
-             print("Email: " + self.email)
-             print("If correct type 'Yes', otherwise type nothing")
+            print("Are These details correct?")
+            print("Name: " + self.name)
+            print("Phone: " + self.phone)
+            print("Email: " + self.email)
+            print("If correct type 'Yes', otherwise type nothing")
+            if input() == "Yes":
+                  Contact_Book[self.name] = "Phone: " + self.phone, "Email: " + self.email
+                  print("Your record has been added")
+                  print(Contact_Book)
+            else:
+                 print("New Record Addition Cancelled") 
+
+class ExtraVender(Vender):
+     def __init__(self, name, phone, email, company, parentalcontact, medical):
+          super().__init__(name, phone, email)
+          self.company = company
+          self.parentalcontact = parentalcontact
+          self.medical = medical
+     def print_extra_vender(self):
+            print("Are These details correct?")
+            print("Name: " + self.name)
+            print("Phone: " + self.phone)
+            print("Email: " + self.email)
+            print("Company: " + self.company)
+            print("Parent Contact: " + self.parentalcontact)
+            print("Medical Condition: " + self.medical)
+            print("If correct type 'Yes', otherwise type nothing")
+            if input() == "Yes":
+                  Contact_Book[self.name] = "Phone: " + self.phone, "Email: " + self.email, "Company: " + self.company, "Parent Contact: " + self.parentalcontact, "Medical Condition: " + self.medical
+                  print("Your record has been added")
+                  print(Contact_Book)
+            else:
+                 print("New Record Addition Cancelled")
+     
+            
 
 
 # Contact Book Global Functions 
@@ -23,11 +53,33 @@ class Vender:
 
 # This Function will allow new venders to be added with their info
 def Add_New_Vender():
-    pass    
+    print("Please enter your full name")
+    name = input()
+    print("Please enter your phone number")
+    phone = input()
+    print("Please enter your email address")
+    email = input()
+    NewVender = Vender(name, phone, email)
+    NewVender.print_vender()  
+
 
 # This function will allow vendors to add extra info they want to provide if the compulsory info isn't enough
 def Add_Extra_Vender_Info():
-    pass
+    print("Please enter your full name")
+    name = input()
+    print("Please enter your phone number")
+    phone = input()
+    print("Please enter your email address")
+    email = input()
+    print("Please enter your company name")
+    company = input()
+    print("Please enter your parental contact number")
+    parentalcontact = input()
+    print("Please enter your medical condition")
+    medical = input()
+    ExtraVender_2 = ExtraVender(name, phone, email, company, parentalcontact, medical)
+    ExtraVender_2.print_extra_vender()
+
 
 # This function will allow "Stellar Events" to access the info the vender via searching by their full name
 def Access_Vender_Info():
