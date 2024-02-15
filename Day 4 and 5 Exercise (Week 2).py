078db2c921c0d35a5f5da02eb63221a086170ee4
sean859 <sean.albertyn04@gmail.com>
2024-02-16 10:58:48 +1300

    Big Changes
    Completed the update, remove, and access, but deleted the add vender and might redo the base class

--- Day 4 and 5 Exercise (Week 2).py
+++ Day 4 and 5 Exercise (Week 2).py
@@ -23,36 +23,38 @@ class Vender:
 
 # This Function will allow new venders to be added with their info
 def Add_New_Vender():
-    print("Please enter your full name")
-    name = input()
-    print("Please enter your phone number")
-    phone = input()
-    print("Please enter your email address")
-    email = input()
-    new_vender = Vender(name, phone, email)
-    new_vender.print_vender()
-    if input() == "Yes":
-         Contact_Book[name] = 'Phone:', phone, 'Email:', email
-         print("Your details have been added to the contact book")
-    else:
-         print("Then please try again")
-         Add_New_Vender()
-Add_New_Vender()
-
-# This function will allow "Stellar Events" to access the info the vender via searching by their name
-def Access_Vender_Info():
-    pass
-
-
-# This function will allow "Stellar Events" to remove any vender and all their info 
-def Remove_Vender():
-    pass
-
-# This function will allow venders to update their info so that its more relevant
-def Update_Vender():
-    pass
+    pass    
 
 # This function will allow vendors to add extra info they want to provide if the compulsory info isn't enough
 def Add_Extra_Vender_Info():
     pass
+
+# This function will allow "Stellar Events" to access the info the vender via searching by their full name
+def Access_Vender_Info():
+    print("Search by full name")
+    search = input()
+    if search in Contact_Book:
+         print(Contact_Book.get(search))
+    else:
+         print("No Record Found")
+
+# This function will allow "Stellar Events" to remove any vender and all their info 
+def Remove_Vender():
+    removethisperson = input("Type the full name of the record you want to delete: ")
+    if removethisperson in Contact_Book:
+        Contact_Book.pop(removethisperson)
+        print(removethisperson + " was removed from record")
+    else:
+         print("Record Not Found")
+
+# This function will allow venders to update their info so that its more relevant
+def Update_Vender():
+    updateoldvender = input("Please type the full name of the vender you want to update: ")
+    if updateoldvender in Contact_Book:
+         Contact_Book.pop(updateoldvender)
+         print("Updating " + updateoldvender)
+         Add_New_Vender()
+         print(updateoldvender + " has been updated")
+    else:
+         print("That Record Does Not Exist")
 
