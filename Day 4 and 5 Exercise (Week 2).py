486ff9a9aa33c3ea4412518b77f5fe965748cf43
sean859 <sean.albertyn04@gmail.com>
2024-02-16 11:49:41 +1300

    Application is finished

--- Day 4 and 5 Exercise (Week 2).py
+++ Day 4 and 5 Exercise (Week 2).py
@@ -22,6 +22,7 @@ class Vender:
             else:
                  print("New Record Addition Cancelled") 
 
+# Where as this class will be used for the Add Extra Vendor Details so that it has the compulsory info including some extra information
 class ExtraVender(Vender):
      def __init__(self, name, phone, email, company, parentalcontact, medical):
           super().__init__(name, phone, email)
@@ -45,11 +46,35 @@ class ExtraVender(Vender):
                  print("New Record Addition Cancelled")
      
             
-
-
 # Contact Book Global Functions 
 # The point of these functions to be used throughout the 
 # code allowing less strain on the code and more tidyness
+
+# This function is the main menu where all the functions below will be accessed depending on the users input
+def ContactBook():
+    print("-----Contact Book-----")
+    print("To select an option press the corrosponding number")
+    print("1. Add New Vender")
+    print("2. Add New Vender (With Addition Information)")
+    print("3. Access Vender Information")
+    print("4. Remove Current Vender Information")
+    print("5. Update Current Vender Information")
+
+    contactbookinput = input()
+
+    if contactbookinput == "1":
+        Add_New_Vender()
+    elif contactbookinput == "2":
+        Add_Extra_Vender_Info()
+    elif contactbookinput == "3":
+        Access_Vender_Info()
+    elif contactbookinput == "4":
+        Remove_Vender()
+    elif contactbookinput == "5":
+        Update_Vender()
+    else:
+        print("Please enter a valid number")
+        ContactBook()
 
 # This Function will allow new venders to be added with their info
 def Add_New_Vender():
@@ -60,8 +85,8 @@ def Add_New_Vender():
     print("Please enter your email address")
     email = input()
     NewVender = Vender(name, phone, email)
-    NewVender.print_vender()  
-
+    NewVender.print_vender() 
+    ContactBook() 
 
 # This function will allow vendors to add extra info they want to provide if the compulsory info isn't enough
 def Add_Extra_Vender_Info():
@@ -79,7 +104,7 @@ def Add_Extra_Vender_Info():
     medical = input()
     ExtraVender_2 = ExtraVender(name, phone, email, company, parentalcontact, medical)
     ExtraVender_2.print_extra_vender()
-
+    ContactBook()
 
 # This function will allow "Stellar Events" to access the info the vender via searching by their full name
 def Access_Vender_Info():
@@ -87,8 +112,10 @@ def Access_Vender_Info():
     search = input()
     if search in Contact_Book:
          print(Contact_Book.get(search))
+         ContactBook()
     else:
          print("No Record Found")
+         ContactBook()
 
 # This function will allow "Stellar Events" to remove any vender and all their info 
 def Remove_Vender():
@@ -96,8 +123,10 @@ def Remove_Vender():
     if removethisperson in Contact_Book:
         Contact_Book.pop(removethisperson)
         print(removethisperson + " was removed from record")
+        ContactBook()
     else:
          print("Record Not Found")
+         ContactBook()
 
 # This function will allow venders to update their info so that its more relevant
 def Update_Vender():
@@ -107,6 +136,9 @@ def Update_Vender():
          print("Updating " + updateoldvender)
          Add_New_Vender()
          print(updateoldvender + " has been updated")
+         ContactBook()
     else:
          print("That Record Does Not Exist")
+         ContactBook()
 
+ContactBook()
