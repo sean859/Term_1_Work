954b1e555c610366e807e1c790ae321cefc4ac3e
sean859 <sean.albertyn04@gmail.com>
2024-02-27 11:40:58 +1300

    Added Binary Search Algorithm
    Completed the binary search code and explaination

--- Day 2 Exercises (Week 4).py
+++ Day 2 Exercises (Week 4).py
@@ -1,4 +1,4 @@
-# Linear Search Algoithm
+# Linear Search Algorithm
 
 # Explained:
 # Linear search is a basic search engine where the user can input a number or word, that will be considered the 'search' value, then the algorithm will take that 'search' value and compare to every value in the list
@@ -21,5 +21,32 @@ def linear_search(search_list, search_value):
     else:
         print("Value Was Not Found")
 linear_search([3, 4, 6, 7, 12, 15, 9, 12], 85)
+
+
+# Binary Search Algorithm
+
+# Explained:
+# The binary search takes a sorted list and creates a 'midpoint' being the number in the very middle of the list, and compares it to the 'search' number if they don't match it then creates a sublist filled with
+# either lower or higher values then the 'midpoint' value if the 'search' value is higher or lower then the 'midpoint' basically if the number you searched is higher then the 'midpoint' it creates a sublist
+# filled the values that are higher then the 'midpoint' and vice versa if its lower, then with this new sublist it repeats the process, making a 'midpoint' with the middle most number comparing it the 'search'
+# value and if its lower or higher creates a new sublist with lower or higher values depending on the search, then it keeps repeating until the 'midpoint' is equal to the 'search' value and the cycle breaks
+
+# Code
+def binary_search(search_list, search_value):
+    begin = 0
+    end = len(search_list) -1
+
+    while begin <= end:
+        midpoint = begin + (end - begin) // 2
+        midpoint_value = search_list[midpoint]
+        if midpoint_value == search_value:
+            return midpoint
+        elif search_value < midpoint_value:
+            end = midpoint - 1
+        else:
+            begin = midpoint + 1
+    return "Value Not Found"
+print(binary_search([12, 24, 30, 42, 55, 63, 75, 82, 91, 99], 12))
+
 
 
