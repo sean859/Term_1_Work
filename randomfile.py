d60aceb764c0971dc13b058e96f5806bd2a278b6
sean859 <sean.albertyn04@gmail.com>
2024-03-07 13:27:12 +1300

    Random code file created
    This is just filler code to push into side_branch

---
+++ randomfile.py
@@ -0,0 +1,6 @@
+# This file is just here to get pushed in the side_branch I made for my github
+
+def randomfunction(randomword: str):
+    for i in range(1,11):
+        print(f"This is just filler code to fill the file {randomword}")
+randomfunction('Apple')
